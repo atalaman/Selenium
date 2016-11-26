e881d36391714109528e1184137d472e0c826f91
atalaman <a.talaman@gmail.com>
2016-11-25 19:25:21 -0500

    Additional changes committed

--- java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -4,9 +4,17 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
@@ -17,7 +25,12 @@ public class MyFirstTest {
 
   @Before
   public void start() {
-    driver = new ChromeDriver();
+    DesiredCapabilities caps = new DesiredCapabilities();
+    caps.setCapability(FirefoxDriver.MARIONETTE, false);
+    FirefoxBinary bin = new FirefoxBinary(new File("d:\\Programs\\Mozilla Firefox\\firefox.exe"));
+    //FirefoxBinary bin = new FirefoxBinary(new File("d:\\Programs\\Nightly\\firefox.exe"));
+    driver = new FirefoxDriver(bin, new FirefoxProfile(), caps);
+    System.out.println(((HasCapabilities) driver).getCapabilities());
     wait = new WebDriverWait(driver, 10);
   }
 
@@ -26,7 +39,7 @@ public class MyFirstTest {
     driver.navigate().to("http://www.google.com");
     driver.findElement(By.name("q")).sendKeys("gismeteo");
     driver.findElement(By.name("btnG")).click();
-    wait.until(titleIs("gismeteo - Поиск в Google"));
+    wait.until(titleIs("gismeteo - Google Search"));
   }
 
   @After
