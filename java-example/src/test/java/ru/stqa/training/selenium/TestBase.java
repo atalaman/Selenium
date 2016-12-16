e426a6ab96208485aecaf302b1e35a2e9d73e18e
atalaman <a.talaman@gmail.com>
2016-12-16 11:37:16 -0500

    Listener has been added

--- java-example/src/test/java/ru/stqa/training/selenium/TestBase.java
+++ java-example/src/test/java/ru/stqa/training/selenium/TestBase.java
@@ -6,9 +6,12 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
@@ -17,12 +20,30 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public abstract class TestBase {
 
-  public WebDriver driver;
+  public EventFiringWebDriver driver;
   public WebDriverWait wait;
+
+  public static class MyListener extends AbstractWebDriverEventListener {
+    @Override
+    public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+      System.out.println(by);
+    }
+
+    @Override
+    public void afterFindBy(By by, WebElement element, WebDriver driver) {
+      System.out.println(by +  " found");
+    }
+
+    @Override
+    public void onException(Throwable throwable, WebDriver driver) {
+      System.out.println(throwable);
+    }
+  }
 
   @Before
   public void start() {
-    driver = new ChromeDriver();
+    driver = new EventFiringWebDriver(new ChromeDriver());
+    driver.register(new MyListener());
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     wait = new WebDriverWait(driver, 10);
   }
